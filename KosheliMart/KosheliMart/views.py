43b4e090e9ba4101cc873de31dea6b9f68f6f384
KeshabNeupane <keshabneupane061@gmail.com>
2025-06-24 22:26:50 +0545

    first commit

---
+++ KosheliMart/KosheliMart/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def Homepage(request):
+    return HttpResponse("home page!")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from new-e-commerce!")
+
+
+if __name__ == "__main__":
+    main()
